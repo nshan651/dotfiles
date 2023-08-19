f8b8cf69b3f38cef7ffd1e2b09e2fe839a3761b5
nshan651 <nickesg44@gmail.com>
2023-08-19 13:27:38 -0500

    Selfhost

--- .local/refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-xrandr --output eDP1 --auto --primary --mode 1920x1080 --rotate normal --output DP1 --auto --mode 1920x1080 --rotate normal --right-of eDP1 --output DP2-3 --auto --mode 1920x1080 --rotate normal --right-of DP1
-
-# Reload
-/bin/sh ~/.config/polybar/launch.sh --blocks
