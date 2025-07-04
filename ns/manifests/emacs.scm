c7d364d78df40df0e003f206ca8a6eb89cef6f82
nshan651 <nickesg44@gmail.com>
2025-07-04 10:57:18 -0500

    Added system deps to emacs.scm manifest.

--- ns/manifests/emacs.scm
+++ ns/manifests/emacs.scm
@@ -1,3 +1,5 @@
+;;; Install this manifest file on a foreign distro with
+;;; `guix package -m emacs.scm' to let guix package your emacs.
 (use-modules (guix packages)
              (guix gexp)
              (gnu packages)
@@ -5,167 +7,175 @@
              (gnu packages emacs-build)
              (gnu packages emacs-xyz)
              (nongnu packages emacs)
-             (gnu packages version-control))
+             (gnu packages version-control)
+             (gnu packages curl)
+             (gnu packages tls)
+             (gnu packages sqlite))
 
 (packages->manifest
-  (list 
-   ;; Emacs itself.
-   emacs
+ (list
+  ;; System-level deps.
+  curl
+  sqlite
+  gnutls
 
-   ;; Themes.
-   emacs-doom-themes
-   emacs-ef-themes
-   emacs-nerd-icons
-   emacs-doom-modeline
-   emacs-org-bullets
-   emacs-perspective
+  ;; Emacs itself.
+  emacs
 
-   ;; Evil mode.
-   emacs-evil
-   emacs-evil-org
-   emacs-evil-collection
-   emacs-general
-   emacs-evil-nerd-commenter
-   emacs-ws-butler
-   emacs-hydra
-   
-   ;; UI.
-   emacs-undo-fu
-   emacs-undo-fu-session
-   emacs-which-key
-   emacs-vertico
-   emacs-corfu
-   emacs-kind-icon
-   emacs-orderless
-   emacs-consult
-   emacs-wgrep
-   emacs-marginalia
-   emacs-embark
-   emacs-helpful
+  ;; Themes.
+  emacs-doom-themes
+  emacs-ef-themes
+  emacs-nerd-icons
+  emacs-doom-modeline
+  emacs-org-bullets
+  emacs-perspective
 
-   emacs-counsel
+  ;; Evil mode.
+  emacs-evil
+  emacs-evil-org
+  emacs-evil-collection
+  emacs-general
+  emacs-evil-nerd-commenter
+  emacs-ws-butler
+  emacs-hydra
 
-   ;; Development.
-   emacs-lsp-mode
-   emacs-lsp-ui
-   emacs-consult-lsp
-   emacs-lsp-treemacs
-   emacs-dap-mode
-   emacs-company
-   emacs-company-box
-   emacs-projectile
+  ;; UI.
+  emacs-undo-fu
+  emacs-undo-fu-session
+  emacs-which-key
+  emacs-vertico
+  emacs-corfu
+  emacs-kind-icon
+  emacs-orderless
+  emacs-consult
+  emacs-wgrep
+  emacs-marginalia
+  emacs-embark
+  emacs-helpful
 
-   ;; Programming Languages/LSP.
-   emacs-yaml-mode
-   emacs-ccls
-   ;emacs-python-mode
-   ;emacs-go-mode
-   ;emacs-rust-mode
+  emacs-counsel
 
-   ;; Latex
-   emacs-latex-preview-pane
+  ;; Development.
+  emacs-lsp-mode
+  emacs-lsp-ui
+  emacs-consult-lsp
+  emacs-lsp-treemacs
+  emacs-dap-mode
+  emacs-company
+  emacs-company-box
+  emacs-projectile
 
-   ;; Misc.
-   emacs-default-text-scale
-   emacs-xclip
-   emacs-avy
-   emacs-ace-window
-   emacs-websocket
+  ;; Programming Languages/LSP.
+  emacs-yaml-mode
+  emacs-ccls
+					;emacs-python-mode
+					;emacs-go-mode
+					;emacs-rust-mode
 
-   ;; Visual fill
-   emacs-visual-fill-column
+  ;; Latex
+  emacs-latex-preview-pane
 
-   emacs-org-appear
+  ;; Misc.
+  emacs-default-text-scale
+  emacs-xclip
+  emacs-avy
+  emacs-ace-window
+  emacs-websocket
 
-   ;; Compatibility with newer Emacs versions.
-   emacs-compat
+  ;; Visual fill
+  emacs-visual-fill-column
 
-   ;; Fonts.
-   emacs-nerd-icons
-   emacs-all-the-icons
+  emacs-org-appear
 
-   ;; Org.
-   emacs-org
-   emacs-org-journal
-   emacs-org-modern
-   emacs-org-pomodoro
-   emacs-org-make-toc
-   emacs-org-roam
-   emacs-org-roam-ui
-   emacs-org-ql
-   emacs-htmlize
-   emacs-logos
+  ;; Compatibility with newer Emacs versions.
+  emacs-compat
 
-   ;; Sqlite3 dependency for org roam.
-   emacs-sqlite3-api
-   emacs-emacsql
+  ;; Fonts.
+  emacs-nerd-icons
+  emacs-all-the-icons
 
-   ;; Web.
-   emacs-ox-hugo
+  ;; Org.
+  emacs-org
+  emacs-org-journal
+  emacs-org-modern
+  emacs-org-pomodoro
+  emacs-org-make-toc
+  emacs-org-roam
+  emacs-org-roam-ui
+  emacs-org-ql
+  emacs-htmlize
+  emacs-logos
 
-   ;; Applications
-   emacs-org-caldav
+  ;; Sqlite3 dependency for org roam.
+  emacs-sqlite3-api
+  emacs-emacsql
 
-   ;; Version control.
-   emacs-magit
-   emacs-magit-todos
-   emacs-forge
-   emacs-git-link
-   emacs-git-gutter
-   emacs-git-gutter-fringe
+  ;; Web.
+  emacs-ox-hugo
 
-   ;; Programming Languages.
-   ;emacs-sly
-   ;emacs-sly-asdf
+  ;; Applications
+  emacs-org-caldav
 
-   emacs-geiser
+  ;; Version control.
+  emacs-magit
+  emacs-magit-todos
+  emacs-forge
+  emacs-git-link
+  emacs-git-gutter
+  emacs-git-gutter-fringe
 
-   emacs-orgalist
-   emacs-markdown-mode
+  ;; Programming Languages.
+					;emacs-sly
+					;emacs-sly-asdf
 
-   emacs-popper
-   emacs-modus-themes
+  emacs-geiser
 
-   emacs-web-mode
-   emacs-skewer-mode
+  emacs-orgalist
+  emacs-markdown-mode
+
+  emacs-popper
+  emacs-modus-themes
+
+  emacs-web-mode
+  emacs-skewer-mode
 
 
-   emacs-flycheck
+  emacs-flycheck
 
-   emacs-yasnippet
-   emacs-yasnippet-snippets
+  emacs-yasnippet
+  emacs-yasnippet-snippets
 
-   emacs-smartparens
+  emacs-smartparens
 
-   emacs-rainbow-delimiters
+  emacs-rainbow-delimiters
 
-   emacs-rainbow-mode
+  emacs-rainbow-mode
 
-   emacs-posframe
+  emacs-posframe
 
-   emacs-a
-   emacs-request
+  emacs-a
+  emacs-request
 
-   ;; AI.
-   emacs-gptel
+  ;; AI.
+  emacs-gptel
 
-   ;; TODO: Move to mail profile
-   emacs-eat
-   emacs-esh-autosuggest
-   emacs-xterm-color
-   emacs-exec-path-from-shell
+  ;; TODO: Move to mail profile
+  emacs-eat
+  emacs-esh-autosuggest
+  emacs-xterm-color
+  emacs-exec-path-from-shell
 
-   emacs-pcmpl-args
+  emacs-pcmpl-args
 
-   emacs-eshell-syntax-highlighting
-   emacs-vterm
+  emacs-eshell-syntax-highlighting
+  emacs-vterm
 
-   emacs-elfeed
+  emacs-elfeed
 
-   emacs-guix
+  emacs-guix
 
-   emacs-daemons
+  emacs-daemons
 
-   emacs-docker
-   emacs-dockerfile-mode
-   ))
+  emacs-docker
+  emacs-dockerfile-mode
+  ))
